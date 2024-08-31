be31057129fc0623289d3622b7c0003f992c355e
yusuga <dev@yusuga.com>
2024-08-31 17:44:50 +0900

    feat: add routes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,8 +2,13 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    // register routes
-    try routes(app)
+  // Serves files from `Public/` directory
+  app.middleware.use(
+    FileMiddleware(
+      publicDirectory: app.directory.publicDirectory
+    )
+  )
+
+  // register routes
+  try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,35 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+  app.get { req async in
+    "It works!"
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("hello") { req async -> String in
+    "Hello, world!"
+  }
+
+  app.get("json") { _ async throws in
+    let json = [
+      "message": "Hello, Vapor!",
+      "status": "success"
+    ]
+    let response = Response(status: .ok)
+    try response.content.encode(json, as: .json)
+    return response
+  }
+
+  app.get("cat") { request async throws in
+    let image = try await request.fileio.collectFile(
+      at: app.directory.publicDirectory.appending("cat.jpg")
+    )
+    var headers = HTTPHeaders()
+    headers.contentType = .jpeg
+
+    return Response(
+      status: .ok,
+      headers: headers,
+      body: .init(buffer: image)
+    )
+  }
 }
